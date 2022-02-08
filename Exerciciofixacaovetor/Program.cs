1499502937fade38dfed72d7d0d1ad6293d6f2de
Rafael Augusto <rafajunior2907@gmail.com>
2022-02-08 16:29:59 -0300

    Add project files.

---
+++ Exerciciofixacaovetor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exerciciofixacaovetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student[] studentVect = new Student[10];
+
+            Console.Write("Quantos quartos serão alugados: ");
+            int numberBadrooms = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < numberBadrooms; i++)
+            {
+                Console.WriteLine($"Aluguel #{i + 1}:");
+                Console.Write("Name: ");
+                string nameStudent = Console.ReadLine();
+                Console.Write("Mail: ");
+                string mailStudent = Console.ReadLine();
+                Console.Write("Badroom: ");
+                int badroom = int.Parse(Console.ReadLine());
+                studentVect[badroom] = new Student(nameStudent, mailStudent);
+            }
+            Console.WriteLine("Occupied Room: ");
+            for(int i = 0; i < 10; i++)
+            {
+                if(studentVect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + studentVect[i]);
+                }
+            }
+
+        }
+    }
+}
---
+++ Exerciciofixacaovetor/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerciciofixacaovetor
+{
+    class Student
+    {
+        public string NameStudent { get; set; }
+        public string EmailStundet { get; set; }
+
+        public Student(string nameStudent, string emailStudent)
+        {
+            NameStudent = nameStudent;
+            EmailStundet = emailStudent;
+        }
+
+        public override string ToString()
+        {
+            return NameStudent + ", " + EmailStundet;
+        }
+    }
+}
